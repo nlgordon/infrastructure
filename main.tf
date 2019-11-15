4ab32117ca0370ba9828a762020bd33b05606000
Nate Gordon <nlgordon@gmail.com>
2019-11-14 22:41:05 -0600

    Adding simple vpc for testing

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 0.12.0"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "nlgordon"
+
+    workspaces {
+      prefix = "testing-"
+    }
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" basic {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "${terraform.workspace}-basic"
+  }
+}
